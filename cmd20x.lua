ee3b0c51efc69433fcdd842f0a0f92ba0e0cb4d0
John lardier <158211153+LLua-u@users.noreply.github.com>
2024-02-25 17:56:54 -0800

    Update and rename cmd19x.lua to cmd20x.lua

--- cmd20x.lua
+++ cmd20x.lua
@@ -1,5 +1,5 @@
 local admins = {"boombongbingy", "tacodoomsday", "100kwadaccount", "noobmankill123", "Partykidcrazy", "Daybot2" , "Partykidcrazy2", "123iloveu3231", "nawalamodfriz_alt", "wegotdatbank"}
-print("Loaded Lua_u's and Quasars rtg admin handler")
+print("Loaded Road to aids")
 function GetPlayer(name)
 	for i, v in pairs(game.Players:GetPlayers()) do
 		if string.match(string.lower(name), string.sub(string.lower(v.Name),0,#name)) or string.match(string.lower(name), string.sub(string.lower(v.DisplayName),0,#name)) then
@@ -234,7 +234,7 @@ function boot(plr)
 			detector.Name = "Detect"
 			task.spawn(function()
 				while task.wait() do
-					detector.CFrame = atplr.Head.CFrame
+					detector.CFrame = atplr.Head.CFrame * CFrame.new(Vector3.new(0, 0, -2000))
 				end
 			end)
 			detector.Size = Vector3.new(1,1,2000)
